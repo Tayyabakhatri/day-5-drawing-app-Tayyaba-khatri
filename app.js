1b6279a6e7709a62bf42994e6efbc4bc1a709de3
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-11-05 23:00:46 +0500

    modified

--- app.js
+++ app.js
@@ -4,30 +4,61 @@ let decrease = document.getElementById("decrease");
 let clear = document.getElementById("clear");
 let size = document.getElementById("size");
 let color = document.getElementById("color");
-// const ctx= canvas.getContext('2d')
+
+const ctx = canvas.getContext('2d');
+
 let sizeElement = 0
-color.value = "black";
-let colorElement = color.value;
+let isPressed = false;
+// color.value = "black";
+
 let x;
 let y;
-let isPressed = false;
-color.value = "black"
 
+color.value = "#23e3eb"
+let colorElement = color.value;
 canvas.addEventListener('mousedown', (e) => {
     isPressed = true
     x = e.offsetX;
-    y = e.offsetY
+    y = e.offsetY;
 });
 canvas.addEventListener('mouseup', (e) => {
     isPressed = false
     x = undefined
     y = undefined
 })
-//icreament and decreament in size 
-function updatesize(){
-    size.innerHTML=sizeElement
+canvas.addEventListener('mousemove', (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+        drowCircle(x2, y2);
+        drawLine(x, y, x2, y2)
+        x = x2;
+        y = y2;
     }
-    //increasing
+})
+function drowCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, sizeElement, 0, Math.PI * 2)
+    ctx.fillStyle = colorElement;
+    ctx.fill()
+}
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = colorElement
+    ctx.lineWidth = sizeElement * 2
+    ctx.stroke()
+}
+
+
+
+
+//icreament and decreament in size 
+function updatesize() {
+    size.innerHTML = sizeElement
+}
+//increasing
 increase.addEventListener('click', () => {
     sizeElement += 2
     if (sizeElement > 30) {
@@ -36,11 +67,14 @@ increase.addEventListener('click', () => {
     updatesize()
 })
 //decreasing
-decrease.addEventListener('click',()=>{
+decrease.addEventListener('click', () => {
     sizeElement -= 2
-    if(sizeElement<1){
-        sizeElement=1
+    if (sizeElement < 1) {
+        sizeElement = 1
     }
     updatesize()
 })
 
+color.addEventListener('change', (e) => colorElement = e.target.value);
+clear.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
+// export{drawLine,drowCircle,updatesize,isPressed,x,y,increase,decrease}
